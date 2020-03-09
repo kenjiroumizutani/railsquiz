28a07e4edfe904b11b49cdd58d8157a3fe52d1e5
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-09 13:05:59 +0000

    3/9復帰様子見

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,20 +3,39 @@ class QuizzesController < ApplicationController
   def index
     @quiz = Quizzes.all
   end
+
+  def new
+    @quiz = Quizzes.find(params[:id])
+    @answer = Answers.new
+    @answer.quizzes_id = @quizzes_id
+  end
+
   
   def edit
-    @quiz = Quizzes.find(params[:id])
-    @answer = @quiz.users
+    @quiz = Quizzes.find(params[:quizzes_id])
+    @answer = Answers.create(create_params)
+    @answer.quizzes_id = @quizzes_id
   end
   
   def update
-    @quiz = Quizzes.find(params[:id])
+    @quiz = Quizzes.find(params[:quizzes_id])
+    @answer = Answers.create(update_params)
+    @answer.quizzes_id = @quizzes_id
     @quiz.update(quizzes_params) 
     # (questions_params)はしたの記述に由来している。answer以外の内容はストロングパラメータではじくよ
   end
   
   private
   def quizzes_params
-    params.permit(:answer,:comment)
+    params.require(:answer).permit(:answer,:comment,:quizzes_id, ).merge(user_id: current_user.id)
   end
+  
+  def create_params
+    params.require(:answer).permit(:answer,:comment,:quizzes_id, ).merge(user_id: current_user.id)
+  end
+  
+  def update_params
+    params.require(:answer).permit(:answer,:comment,:quizzes_id, ).merge(user_id: current_user.id)
+  end
+  
 end
